fcafedf00a6a073450a317d5b869384c50c7b406
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2023-05-09 20:25:36 +0200

    use script to extract plugin names

--- tools/extract_plugin_names.py
+++ tools/extract_plugin_names.py
@@ -1,6 +1,5 @@
 import sys
 import toml
-import json
 
 if len(sys.argv) != 2:
     print("Usage: python toml_to_json.py <filename>")
@@ -10,5 +9,6 @@ filename = sys.argv[1]
 with open(filename, "r") as f:
     toml_data = toml.load(f)
 
-json_data = json.dumps(toml_data)
-print(json_data)
+result = toml_data["project"]["entry-points"]["molecule.driver"].keys()
+for i in result:
+    print(i)
--- tools/generate-templates.sh
+++ tools/generate-templates.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 rm -rf test/roles/* || true
 mkdir -p test/roles
 
-while IFS='' read -r line; do DRIVER_NAMES+=("$line"); done < <(python "tools/toml_to_json.py" "pyproject.toml" | jq -rc '.project."entry-points"."molecule.driver" | keys[]')
+while IFS='' read -r line; do DRIVER_NAMES+=("$line"); done < <(python "tools/extract_plugin_names.py" "pyproject.toml")
 
 cd test/roles
 for DRIVER_NAME in "${DRIVER_NAMES[@]}"; do
