fdc748ffa7b72686f4f99196c8b528167eccf40f
Daniel <48068081+danielpodwysocki@users.noreply.github.com>
2024-09-27 22:39:14 +0200

    fix #239 , use python-style templating instead of Jinja in the plugins login_cmd_template (#240)
    fix #239 , use python-style templating instead of Jinja in all drivers login_cmd_template
    
    This file assumes Jinja templating for the port parameter and passes a Jinja-style "{{ port }}".
    
    https://github.com/ansible/molecule/blob/main/src/molecule/command/login.py#L105
    
    When it reaches molecule, it is subsituted in  this file and is done by python calling `.format()` on the string.
    That causes it to not render correctly and gives users issues running
    molecule login.
    
    ref: https://github.com/ansible-community/molecule-plugins/issues/239
    
    Fixed all affected plugins.

--- src/molecule_plugins/ec2/driver.py
+++ src/molecule_plugins/ec2/driver.py
@@ -195,10 +195,10 @@ class EC2(Driver):
             connection_options = " ".join(self.ssh_connection_options)
 
             return (
-                "ssh {{address}} "
-                "-l {{user}} "
-                "-p {{port}} "
-                "-i {{identity_file}} "
+                "ssh {address} "
+                "-l {user} "
+                "-p {port} "
+                "-i {identity_file} "
                 f"{connection_options}"
             )
 
